4b73d4a6f4e9aa7cefa2dd1d28b4ebad1b4958fa
Luke Guan <lukeguan@outlook.com>
2022-03-22 23:04:11 -0700

    ApplicationContext

--- src/main/java/com/in28minutes/sprintboot/basics/sprintbootin10steps/SprintbootIn10StepsApplication.java
+++ src/main/java/com/in28minutes/sprintboot/basics/sprintbootin10steps/SprintbootIn10StepsApplication.java
@@ -2,12 +2,17 @@ package com.in28minutes.sprintboot.basics.sprintbootin10steps;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class SprintbootIn10StepsApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SprintbootIn10StepsApplication.class, args);
+		ApplicationContext applicationContext = SpringApplication.run(SprintbootIn10StepsApplication.class, args);
+		
+		for (String name : applicationContext.getBeanDefinitionNames()) {
+			System.out.println(name);
+		}
 	}
 
 }
